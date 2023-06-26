5412cabe771620930e02b77b57a090dc4aad05e2
Sinchit Rajput <sinchitrajput@gmail.com>
2023-06-26 23:21:22 +0530

    Higher than 75 marks and Employee

---
+++ Higher_Than_75_Marks-Employee.sql
@@ -0,0 +1,30 @@
+/*
+Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+Input Format
+The STUDENTS table is described as follows:
+https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+*/
+Select Name
+From Students
+Where Marks > 75
+Order By Right(Name,3), ID
+
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+Input Format
+The Employee table containing employee data for a company is described as follows:
+*/
+Select Name
+From Employee
+Order By Name
+
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+Input Format
+The Employee table containing employee data for a company is described as follows:
+*/
+Select Name
+From Employee
+Where Salary>2000 and months<10
+Order By employee_id
+
