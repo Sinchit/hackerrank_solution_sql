db8c3ea03cfdc3e0829bbc035f2cc5e6900f75ff
Sinchit Rajput <sinchitrajput@gmail.com>
2023-06-26 21:19:35 +0530

    Aggregation

---
+++ Aggregation.sql
@@ -0,0 +1,21 @@
+/*
+Query a count of the number of cities in CITY having a Population larger than .
+Input Format
+The CITY table is described as follows:
+https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+*/
+Select Count(DISTINCT ID)
+From CITY
+Where POPULATION > 100000
+
+/*
+Query the total population of all cities in CITY where District is California.
+Input Format
+
+The CITY table is described as follows:
+https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+*/
+
+Select Sum(POPULATION)
+From CITY
+Where DISTRICT Like "California"
