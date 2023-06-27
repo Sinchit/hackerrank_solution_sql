cf5cbf13d4f82aad78bda9082fb870d0bf73a95e
Sinchit Rajput <sinchitrajput@gmail.com>
2023-06-27 22:52:36 +0530

    Ollivanders Inventory

---
+++ Ollivanders_Inventory.sql
@@ -0,0 +1,16 @@
+/****************************MS SQL SERVER
+/*
+Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age. Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. If more than one wand has same power, sort the result in order of descending age.
+https://www.hackerrank.com/challenges/harry-potter-and-wands/problem?h_r=next-challenge&h_r=next-challenge&h_v=zen&h_v=zen&isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+*/
+
+SELECT id, age, coins_needed, power 
+FROM ( SELECT id, age, coins_needed, power, 
+      RANK() OVER (PARTITION BY power,age ORDER BY coins_needed) as ranking 
+      FROM wands w 
+      JOIN wands_property p 
+      ON w.code = p.code 
+      WHERE is_evil = 0 ) as temp 
+WHERE ranking = 1 
+ORDER BY power DESC, age DESC;
