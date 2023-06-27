594c4e365da6b98f63189d484c5547deefb5b491
Sinchit Rajput <sinchitrajput@gmail.com>
2023-06-27 22:53:36 +0530

    Updated Binary Tree and Triangle

--- Type_of_Triangles.sql
+++ Type_of_Triangles.sql
@@ -1,4 +1,3 @@
-
 /****************************MS SQL SERVER
 /*
 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
